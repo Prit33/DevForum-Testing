f875452d6078395463cb081b8e27e7281b682073
Prit <74133659+Prit33@users.noreply.github.com>
2023-10-16 02:39:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -62,8 +62,8 @@ app.use((req, res, next) => {
 });
 app.use(express.json())
 app.use(cookieParser())
-// app.use(cors({ origin: "http://localhost:5173/", credentials: true }))
-app.use(cors());
+app.use(cors({ origin: "https://dev-forum-eosin.vercel.app/", credentials: true }))
+// app.use(cors());
 
 // app.use(cors())
 
