720f1cb1889a6542f99e626a78c69d3953840adf
Prit <74133659+Prit33@users.noreply.github.com>
2023-10-15 22:43:08 +0530

    Update verifyToken.js

--- verifyToken.js
+++ verifyToken.js
@@ -1,15 +1,20 @@
 const jwt=require('jsonwebtoken')
+// const cookieParser = require('cookie-parser');
 
 const verifyToken=(req,res,next)=>{
-    // console.log(req.cookies)
-    const token=req.cookies.token
-    // console.log(req.headers.authorization)
-    if(!token){
+    // console.log(req.cookies.token)
+    
+    const token=req.cookies.token;      
+    const tokenValue = req.headers.cookie.split('=')[1];    // doing another way bcoz of deployment problem
+    console.log(tokenValue);
+
+
+    if(!tokenValue){
         return res.status(401).json("You are not authenticated!")
     }
-    jwt.verify(token,process.env.SECRET,async (err,data)=>{
+    jwt.verify(tokenValue,process.env.SECRET,async (err,data)=>{
         if(err){
-            return res.status(403).json("Token is not valid!")
+            return res.status(403).json("tokenValue is not valid!")
         }
         // console.log(data);
         req.userId=data._id
