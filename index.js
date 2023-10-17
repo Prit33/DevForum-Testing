e5f9b2ad02b0a8c45e3958ea4acf9524b8af4f12
Prit <74133659+Prit33@users.noreply.github.com>
2023-10-17 22:28:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.setHeader(
         "Access-Control-Allow-Origin",
-        "http://localhost:5173/"
+        "*"
     );
     
     res.setHeader(
